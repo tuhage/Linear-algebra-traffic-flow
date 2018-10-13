ed579b8521e9f6abe7480fd4e3539e2d459e6ea9
tuhage <tuhage@gmail.com>
2018-10-13 21:12:27 +0300

    First

---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: tuhage
+ *
+ * Created on 13 Ekim 2018 Cumartesi, 21:08
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
